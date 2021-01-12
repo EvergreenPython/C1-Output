185fdc45673c956981863dbe3b7e8a58c84265f3
PythonEESD2021 <ctran@eesd.org>
2021-01-12 00:08:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a comment, comments are not run by the compiler
+# Output: Information that the computer prints out, what you see printed on the right hand side
+
+print("Hello World")
+
+# Name, grade, favorite book, and favorite superhero
+
+print("My name is Lalyn Inguito")
+print("I am in 16th grade.")
+print("My favorite book is 'Fundamentals of Material Balances'.")
+print("My favorite superhero is IRON MAN!!!")
+
+print("Hello")
+print("World")
+
+# Concatenation: add two strings together using '+'
+
+print("Hello" + " World")
